8d9eb4bb88020ea851afbe03047f579dab457071
Steven D Milton <55567912+Steven-Milton-CS365@users.noreply.github.com>
2019-11-22 12:13:31 -0500

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        [Required]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
